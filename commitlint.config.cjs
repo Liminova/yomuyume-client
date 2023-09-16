b5904db69f4a277bb379709328ef463a594a50f3
Delnegend <kiennguyen19323@gmail.com>
2023-09-17 02:36:52 +0700

    build: :package: cleanup commitlint config

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,7 +1,3 @@
-/** @type {import('cz-git').UserConfig} */
 module.exports = {
-    prompt: {
-        useEmoji: true
-    },
-    extends: ['@commitlint/config-conventional']
-}
+    extends: ["@commitlint/config-conventional"],
+};
