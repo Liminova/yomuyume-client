60c837616187041d5c454390d337eb76b09abb93
Delnegend <kiennguyen19323@gmail.com>
2023-10-25 11:45:15 +0700

    refactor(store.ts): :recycle: rearrange im/exports

--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,4 @@
-import AccountSettings from "./pages/AccountSettings/_AccountSettings.vue";
-import ContinueReading from "./pages/ContinueReading/_ContinueReading.vue";
-import Home from "./pages/Home/_Home.vue";
-import Library from "./pages/Library/_Library.vue";
-import RecentlyAdded from "./pages/RecentlyAdded/_RecentlyAdded.vue";
 import { ref } from "vue";
-import { createRouter, createWebHistory } from "vue-router";
 
 function vibrate(): boolean {
 	navigator.vibrate(8);
@@ -13,4 +7,4 @@ function vibrate(): boolean {
 
 const isNavDrawerLarge = ref(true);
 
-export { routes, router, vibrate, isNavDrawerLarge, Routes };
+export { vibrate, isNavDrawerLarge };
