2236bae3b9c6cddeebdea704859c79185c88d005
Delnegend <kiennguyen19323@gmail.com>
2023-10-25 11:56:36 +0700

    refactor: :recycle: account-settings, continue-reading, recently-added with new NavDrawerWrapper

--- src/pages/AccountSettings/_AccountSettings.vue
+++ src/pages/AccountSettings/_AccountSettings.vue
@@ -1,1 +1,15 @@
-<template>Account Settings</template>
+<script setup lang="ts">
+import { onMounted } from "vue";
+import NavDrawerWrapper from "../../components/NavDrawerWrapper.vue";
+import { isNavDrawerLarge } from "../../store";
+
+onMounted(() => {
+	if (window.innerWidth < 1280) {
+		isNavDrawerLarge.value = false;
+	}
+});
+</script>
+
+<template>
+	<NavDrawerWrapper> Account Settings </NavDrawerWrapper>
+</template>
--- src/pages/ContinueReading/_ContinueReading.vue
+++ src/pages/ContinueReading/_ContinueReading.vue
@@ -1,1 +1,15 @@
-<template>Continue Reading</template>
+<script setup lang="ts">
+import NavDrawerWrapper from "../../components/NavDrawerWrapper.vue";
+import { isNavDrawerLarge } from "../../store";
+import { onMounted } from "vue";
+
+onMounted(() => {
+	if (window.innerWidth < 1280) {
+		isNavDrawerLarge.value = false;
+	}
+});
+</script>
+
+<template>
+	<NavDrawerWrapper> Continue reading </NavDrawerWrapper>
+</template>
