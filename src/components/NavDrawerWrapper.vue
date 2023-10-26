a07515a34c16c71dc413bacccc0f8cadb92fb6f6
Delnegend <kiennguyen19323@gmail.com>
2023-10-26 12:47:33 +0700

    feat(NavDrawerWrapper): :sparkles: should've commit earlier

--- src/components/NavDrawerWrapper.vue
+++ src/components/NavDrawerWrapper.vue
@@ -2,13 +2,40 @@
 import NavDrawerLarge from "./NavDrawerLarge/_NavDrawerLarge.vue";
 import NavDrawerSmall from "./NavDrawerSmall/_NavDrawerSmall.vue";
 import TopBar from "./TopBar/_TopBar.vue";
-import { isNavDrawerLarge } from "../store";
+import { isNavDrawerLarge, isTopBarVisible } from "../store";
+import { onMounted } from "vue";
 
 const props = defineProps({
 	class: { type: String, default: "" },
 });
 
-/** */
+isTopBarVisible.value = true;
+onMounted(() => {
+	let prevScrollPos = window.pageYOffset;
+
+	window.onscroll = () => {
+		if (window.innerWidth >= 1024) {
+			return;
+		}
+
+		const htmlElement = document.querySelector("html");
+
+		if (htmlElement === null) {
+			return;
+		}
+
+		const currentScrollPos = -htmlElement.getBoundingClientRect().top;
+
+		if (prevScrollPos > currentScrollPos || currentScrollPos < 100) {
+			isTopBarVisible.value = true;
+		} else {
+			isTopBarVisible.value = false;
+			isNavDrawerLarge.value = false;
+		}
+
+		prevScrollPos = currentScrollPos;
+	};
+});
 </script>
 
 <template>
