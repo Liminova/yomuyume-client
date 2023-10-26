985cd63112a1866b2990700aff3b29946784e92f
Delnegend <kiennguyen19323@gmail.com>
2023-10-26 11:36:53 +0700

    feat(tailwind.config): :sparkles: random-translate animation for recommended bg

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,19 @@ export default {
     darkMode: "class",
     content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            animation: {
+                'random-translate': 'random-translate 10s infinite',
+            },
+            keyframes: {
+                'random-translate': {
+                    '0%, 100%': { transform: 'translateX(0) scale(1.2)' },
+                    '25%': { transform: 'translateX(10%) translateY(10%) scale(1.5)' },
+                    '50%': { transform: 'translateX(-10%) translateY(-10%) scale(1.5)' },
+                    '75%': { transform: 'translateX(10%) translateY(-10%) scale(1.5)' },
+                },
+            }
+        },
     },
     plugins: [],
 };
