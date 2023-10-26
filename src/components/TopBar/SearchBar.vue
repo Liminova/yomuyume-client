b25151dab206ef4d5931a634f8d4ce4f5d2ad8d1
Delnegend <kiennguyen19323@gmail.com>
2023-10-26 11:44:45 +0700

    feat(SearchBar): :sparkles: turn into a link navigate to search route

--- src/components/TopBar/SearchBar.vue
+++ src/components/TopBar/SearchBar.vue
@@ -1,31 +1,34 @@
 <script setup lang="ts">
-const props = defineProps({
-	placeholder: { type: String, default: "Search" },
-});
-
-/** */
+import { Routes } from "../../router";
+import { vibrate } from "../../store";
+import "@material/web/ripple/ripple.js";
 </script>
 
 <template>
-	<div>
+	<router-link
+		:to="Routes.Search"
+		class="elevation-1 relative overflow-hidden rounded-full"
+		@click="vibrate"
+	>
+		<md-ripple></md-ripple>
 		<div
-			class="hidden h-12 max-w-xs flex-row items-center justify-between rounded-full bg-[--md-sys-color-surface-container-high] pl-4 pr-3 md:flex"
+			class="w-xs hidden h-12 flex-row items-center justify-between rounded-full bg-[--md-sys-color-surface-container-high] pl-4 pr-3 md:flex"
 		>
-			<div class="flex items-center justify-center gap-4">
+			<div class="flex w-full items-center justify-center gap-4">
 				<i
 					class="fa-regular fa-magnifying-glass text-xl text-[color:var(--md-sys-color-on-surface-variant)]"
 				></i>
-				<input
-					id=""
-					type="text"
-					name=""
-					:placeholder="props.placeholder"
-					class="bg-transparent text-[color:var(--md-sys-color-on-surface-variant)]"
-				/>
+				<div
+					class="bg-transparent pr-24 text-[color:var(--md-sys-color-on-surface-variant)]"
+				>
+					Search
+				</div>
 			</div>
 		</div>
-		<i
-			class="fa-regular fa-magnifying-glass mr-3 block text-2xl text-[color:var(--md-sys-color-on-surface-variant)] md:hidden"
-		></i>
-	</div>
+		<div class="flex h-12 w-12 items-center justify-center md:hidden">
+			<i
+				class="fa-regular fa-magnifying-glass text-2xl text-[color:var(--md-sys-color-on-surface-variant)]"
+			></i>
+		</div>
+	</router-link>
 </template>
