727e4821047e59683a31067175b165b5736bcb52
Delnegend <kiennguyen19323@gmail.com>
2023-10-27 15:29:25 +0700

    feat(App.vue): :sparkles: check valid session key to decide route to /auth

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,29 @@
 <script setup lang="ts">
+import { router } from "./router";
+import { ref } from "vue";
 
 const isBrowserSafari = ref(/^((?!chrome|android).)*safari/iu.test(navigator.userAgent));
+
+const cookies: Record<string, string> = (() => {
+	const cookies: Record<string, string> = {};
+
+	for (const cookie of document.cookie.split(";")) {
+		const [key, value] = cookie.split("=");
+
+		cookies[key.trim()] = value;
+	}
+
+	return cookies;
+})();
+
+if (localStorage.getItem("instance-address") === null || !cookies.session) {
+	// TODO: remove this if false on production
+	if (false) {
+		router.push("/auth").catch(() => {
+			/** */
+		});
+	}
+}
 
 /** */
 </script>
