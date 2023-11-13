9ba5929ccc94ebc3bddbdd0cb00034dd5460c711
Delnegend <kiennguyen19323@gmail.com>
2023-11-13 17:55:20 +0700

    build(vite.config): :package: add "swiper-" to isCustomElem

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue(
-        { template: { compilerOptions: { isCustomElement: tag => tag.startsWith('md-') } } }
-    )]
+        {
+            template: {
+                compilerOptions: {
+                    isCustomElement: tag => {
+                        const customElements = ['md-', 'swiper-'];
+                        return customElements.some((customElement) => tag.startsWith(customElement));
+                    }
+                }
+            }
+        }
+        )]
 });
