eb86544f2fd5e2ddcd32aa5df2cd90f0086c46f5
Delnegend <kiennguyen19323@gmail.com>
2023-11-19 22:32:38 +0700

    feat: :sparkles: newly-updated page

---
+++ src/pages/NewlyUpdated/_NewlyUpdated.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import NavDrawerWrapper from "../../components/NavDrawerWrapper/_NavDrawerWrapper.vue";
+</script>
+
+<template>
+	<NavDrawerWrapper> Newly updated </NavDrawerWrapper>
+</template>
