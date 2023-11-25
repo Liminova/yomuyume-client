4410ac4631c0cc9cc328305de9bc3fbcbebbf439
Delnegend <kiennguyen19323@gmail.com>
2023-11-26 00:13:51 +0700

    refactor: :recycle: rm prettier; manually format w/ 4 tabs; mv inline css to embeded in index.html

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,16 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue(
-        {
-            template: {
-                compilerOptions: {
-                    isCustomElement: tag => {
-                        const customElements = ['md-', 'swiper-'];
-                        return customElements.some((customElement) => tag.startsWith(customElement));
-                    }
-                }
-            }
-        }
-        )]
+	plugins: [vue(
+		{
+			template: {
+				compilerOptions: {
+					isCustomElement: tag => {
+						const customElements = ['md-', 'swiper-'];
+						return customElements.some((customElement) => tag.startsWith(customElement));
+					}
+				}
+			}
+		}
+	)]
 });
