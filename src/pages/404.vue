9e26a8f3760f36c5a502d3fa6601171096ab3e1d
Delnegend <kiennguyen19323@gmail.com>
2023-12-12 13:24:05 +0700

    style(404): :lipstick: lint

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -2,6 +2,7 @@
 import Routes from "../utils/variables/routes";
 import { inject } from "vue";
 import type { Router } from "vue-router";
+
 const router = inject("router", {}) as Router;
 
 /** */
@@ -9,10 +10,11 @@ const router = inject("router", {}) as Router;
 
 <template>
 	<div class="relative m-0 h-screen overflow-hidden bg-black">
-		<img class="h-full w-full object-cover bg-black" src="/jerry.jpg" alt="jerry" />
+		<img class="h-full w-full bg-black object-cover" src="/jerry.jpg" alt="jerry" />
 		<button
 			class="absolute left-0 top-0 h-full w-full bg-black/50 text-white"
-			@click="router.push(Routes.Home)">
+			@click="router.push(Routes.Home)"
+		>
 			Back to home
 		</button>
 	</div>
