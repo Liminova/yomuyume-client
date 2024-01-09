43717cb904c2637215555f2c38625572b1e393a3
Delnegend <kiennguyen19323@gmail.com>
2024-01-10 02:42:45 +0700

    feat(pages/Home): :sparkles: implement api

--- src/pages/Home/_Home.vue
+++ src/pages/Home/_Home.vue
@@ -3,28 +3,55 @@ import CardRecommend from "./CardRecommend.vue";
 import CarouselWrapper from "./CarouselWrapper.vue";
 import HomeTitle from "./HomeTitle.vue";
 import { coverHeight, recommendsContainerHeight, gapPixel } from "./measurements";
+import indexApi from "../../api";
+import fileApiUrl from "../../api/file";
 import ItemCard from "../../components/ItemCard.vue";
 import NavDrawerWrapper from "../../components/NavDrawerWrapper/_NavDrawerWrapper.vue";
+import SnackBar from "../../components/SnackBar.vue";
 import imageAutoResizer from "../../utils/functions/imageAutoResizer";
-import { randomRecommends, titles } from "../../utils/variables/random";
 import Routes from "../../utils/variables/routes";
 import { register } from "swiper/element/bundle";
 import { onMounted, ref } from "vue";
+import type { FilterTitleResponseBody } from "../../api";
+import type { Ref } from "vue";
 
 register();
 
 const carouselContainerRef = ref<HTMLElement | null>(null);
+const snackbarMessage = ref("");
 
 onMounted(() => {
 	imageAutoResizer(carouselContainerRef, coverHeight, undefined, gapPixel, 3, 4);
 });
 
-const continueReadingItems = titles.slice(0, 25);
+const recommendsItems: Ref<Array<FilterTitleResponseBody>> = ref([]);
+const recentlyUpdatedItems: Ref<Array<FilterTitleResponseBody>> = ref([]);
+const newlyAddedItems: Ref<Array<FilterTitleResponseBody>> = ref([]);
+const completedStoriesItems: Ref<Array<FilterTitleResponseBody>> = ref([]);
+
+void Promise.all([
+	indexApi.filter({ keywords: [""], limit: 10 }, snackbarMessage),
+	indexApi.filter(
+		{ keywords: [""], limit: 10, sort_by: "update date", sort_order: "descending" },
+		snackbarMessage
+	),
+	indexApi.filter(
+		{ keywords: [""], limit: 10, sort_by: "add date", sort_order: "descending" },
+		snackbarMessage
+	),
+	indexApi.filter({ keywords: [""], limit: 10, is_finished: true }, snackbarMessage),
+]).then(([recommends, recentlyUpdated, newlyAdded, completedStories]) => {
+	recommendsItems.value = recommends;
+	recentlyUpdatedItems.value = recentlyUpdated;
+	newlyAddedItems.value = newlyAdded;
+	completedStoriesItems.value = completedStories;
+});
 
 /** */
 </script>
 
 <template>
+	<SnackBar :message="snackbarMessage" @close="snackbarMessage = ''" />
 	<NavDrawerWrapper
 		class="mt-3 flex w-full select-none flex-col gap-7 px-6 lg:mt-0 lg:pl-0 lg:pr-3"
 	>
@@ -34,39 +61,102 @@ const continueReadingItems = titles.slice(0, 25);
 			:autoplay-delay="5000"
 			:autoplay-disable-on-interaction="false"
 		>
-			<swiper-slide v-for="(title, index) in randomRecommends" :key="title.id">
+			<swiper-slide v-for="(title, index) in recommendsItems" :key="title.id">
 				<CardRecommend
-					:author="title.author"
-					:cover-image="title.cover"
-					:description="title.description"
-					:tags="title.tags"
+					:author="title.author ?? 'Unknown'"
+					:cover="{
+						src: fileApiUrl.thumbnail(title.id),
+						width: title.width,
+						height: title.height,
+						blurhash: title.blurhash,
+					}"
+					:description="''"
+					:tags="[]"
 					:title="title.title"
 					:is-first-title="index === 0"
-					:is-last-title="index === randomRecommends.length - 1"
+					:is-last-title="index === recommendsItems.length - 1"
 					:title-id="title.id"
 				/>
 			</swiper-slide>
 		</swiper-container>
 
-		<HomeTitle :path="Routes.ContinueReading">Continue reading</HomeTitle>
+		<!-- <HomeTitle :path="Routes.ContinueReading">Continue reading</HomeTitle>
 		<CarouselWrapper>
 			<swiper-slide v-for="title in continueReadingItems" :key="title.id">
 				<ItemCard
 					:key="title.id"
-					:author="title.author"
-					:cover="title.cover"
+					:author="title.author ?? 'Unknown'"
+					:cover="{
+						src: fileApi.thumbnail(title.id),
+						width: title.width,
+						height: title.height,
+						blurhash: title.blurhash,
+					}"
 					:cover-height="coverHeight"
-					:progress="title.pageRead"
+					:progress="title.page_read ? title.page_read / title.page_count : 0"
+					:title-id="title.id"
+					:title="title.title" />
+			</swiper-slide>
+		</CarouselWrapper> -->
+
+		<HomeTitle :path="Routes.RecentlyUpdated">Recently updated</HomeTitle>
+		<CarouselWrapper>
+			<swiper-slide v-for="title in recentlyUpdatedItems" :key="title.id">
+				<ItemCard
+					:key="title.id"
+					:author="title.author ?? 'Unknown'"
+					:cover="{
+						src: fileApiUrl.thumbnail(title.id),
+						width: title.width,
+						height: title.height,
+						blurhash: title.blurhash,
+					}"
+					:cover-height="coverHeight"
+					:progress="title.page_read ? title.page_read / title.page_count : 0"
 					:title-id="title.id"
 					:title="title.title"
 				/>
 			</swiper-slide>
 		</CarouselWrapper>
-
-		<HomeTitle :path="Routes.RecentlyUpdated">Recently updated</HomeTitle>
 		<HomeTitle :path="Routes.NewlyAdded">Newly added</HomeTitle>
+		<CarouselWrapper>
+			<swiper-slide v-for="title in newlyAddedItems" :key="title.id">
+				<ItemCard
+					:key="title.id"
+					:author="title.author ?? 'Unknown'"
+					:cover="{
+						src: fileApiUrl.thumbnail(title.id),
+						width: title.width,
+						height: title.height,
+						blurhash: title.blurhash,
+					}"
+					:cover-height="coverHeight"
+					:progress="title.page_read ? title.page_read / title.page_count : 0"
+					:title-id="title.id"
+					:title="title.title"
+				/>
+			</swiper-slide>
+		</CarouselWrapper>
 		<HomeTitle :path="Routes.CompletedStories">Completed stories</HomeTitle>
-		<HomeTitle :path="Routes.CompletedReads">Completed reads</HomeTitle>
+		<CarouselWrapper>
+			<swiper-slide v-for="title in completedStoriesItems" :key="title.id">
+				<ItemCard
+					:key="title.id"
+					:author="title.author ?? 'Unknown'"
+					:cover="{
+						src: fileApiUrl.thumbnail(title.id),
+						width: title.width,
+						height: title.height,
+						blurhash: title.blurhash,
+					}"
+					:cover-height="coverHeight"
+					:progress="title.page_read ? title.page_read / title.page_count : 0"
+					:title-id="title.id"
+					:title="title.title"
+				/>
+			</swiper-slide>
+		</CarouselWrapper>
+		<!-- <HomeTitle :path="Routes.CompletedReads">Completed reads</HomeTitle> -->
 
 		<div ref="carouselContainerRef" class="w-full"></div>
 	</NavDrawerWrapper>
