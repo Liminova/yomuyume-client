82b8e82b7c0981fafcbc3e44b404dc3a09a66833
Delnegend <kiennguyen19323@gmail.com>
2024-01-23 08:31:43 +0700

    refactor(components/Toggle): :recycle: utilize transitionend; re-organize classes

--- components/Toggle.vue
+++ components/Toggle.vue
@@ -10,28 +10,53 @@ const props = defineProps({
 	},
 });
 
-const combinedShow = `${props.horizontal ? "grid-cols-[1fr]" : "grid-rows-[1fr]"} opacity-100`;
+const showClass = props.horizontal ? "show-horizontal" : "show-vertical";
+const hideClass = props.horizontal ? "hide-horizontal" : "hide-vertical";
 
-const combinedHide = `${
-	props.horizontal ? "grid-cols-[0fr]" : "grid-rows-[0fr]"
-} overflow-hidden opacity-0`;
+const isShowing = ref(props.show);
 
-/** */
+watchEffect(() => {
+	if (props.show) {
+		isShowing.value = true;
+	}
+});
 </script>
 
 <template>
 	<div
+		v-if="isShowing"
+		ref="container"
 		class="grid"
-		:class="props.show ? combinedShow : combinedHide"
+		:class="props.show ? showClass : hideClass"
 		:style="{
 			pointerEvents: props.show ? 'auto' : 'none',
 			transitionProperty: 'grid-template-columns, grid-template-rows, opacity',
 			transitionDuration: '150ms',
 			transitionTimingFunction: 'cubic-bezier(0.4, 0, 0.2, 1)',
 		}"
+		@transitionend="isShowing = props.show"
 	>
 		<div :class="props.horizontal ? 'min-w-0' : 'min-h-0'">
 			<slot />
 		</div>
 	</div>
 </template>
+
+<style scoped>
+.show-vertical {
+	opacity: 1;
+	grid-template-rows: 1fr;
+}
+.show-horizontal {
+	opacity: 1;
+	grid-template-columns: 1fr;
+}
+.hide-vertical {
+	opacity: 0;
+	grid-template-rows: 0fr;
+}
+.hide-horizontal {
+	opacity: 0;
+	grid-template-columns: 0fr;
+}
+</style>
